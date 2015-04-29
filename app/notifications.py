f8dabb4cef25bcf2ea4d848e29f3ac1204194dab
cthtuf <gdazeysinc@gmail.com>
2015-04-29 23:59:06 +0300

    add crossdomain support to notifications

--- app/notifications.py
+++ app/notifications.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from views import save_lang, save_loc, save_curr, get_lang, get_loc, get_curr
+from views import save_lang, save_loc, save_curr, get_lang, get_loc, get_curr, crossdomain
 from app import db
 from models import *
 from flask import render_template, request, jsonify, session, abort, redirect, url_for
@@ -9,6 +9,7 @@ from flask.ext.babel import gettext
 import sys
 
 #for /<string:language_suffix>/<string:location_suffix/getnotifications/
+@crossdomain
 def notifications_list(language_suffix, location_suffix):
 	return jsonify({
 		'lang' : language_suffix,
