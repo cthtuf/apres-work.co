63989f83431f170ed6c6219b2c544a8b9ee36281
cthtuf <gdazeysinc@gmail.com>
2015-05-20 22:04:16 +0300

    sync db_migrations, static and db

---
+++ db_repository/versions/059_migration.py
@@ -0,0 +1,40 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+camp_contact_useful_page = Table('camp_contact_useful_page', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('order_index', Integer),
+    Column('en_link', String(length=255)),
+    Column('ru_link', String(length=255)),
+    Column('fr_link', String(length=255)),
+    Column('es_link', String(length=255)),
+    Column('icon_class', String(length=20)),
+    Column('in_new_window', Boolean),
+    Column('en_caption', String(length=1000)),
+    Column('ru_caption', String(length=1000)),
+    Column('fr_caption', String(length=1000)),
+    Column('es_caption', String(length=1000)),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['camp_contact_useful_page'].columns['en_link'].create()
+    post_meta.tables['camp_contact_useful_page'].columns['ru_link'].create()
+    post_meta.tables['camp_contact_useful_page'].columns['fr_link'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['camp_contact_useful_page'].columns['en_link'].drop()
+    post_meta.tables['camp_contact_useful_page'].columns['ru_link'].drop()
+    post_meta.tables['camp_contact_useful_page'].columns['fr_link'].drop()
